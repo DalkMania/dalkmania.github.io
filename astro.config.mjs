85969e81cf188d9020caa4c8464625465bc55719
Niklas Dahlqvist <dalkmania@gmail.com>
2023-10-22 09:38:15 -0700

    Formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +13,5 @@ export default defineConfig({
       },
     }),
   ],
-});
+  plugins: ["prettier-plugin-astro"],
+})
