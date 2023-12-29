84c9bf963f24696fcd48656c9085dc773cdc9c8d
Niklas Dahlqvist <dalkmania@gmail.com>
2023-12-29 15:44:02 +0100

    Some Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from "astro/config"
-
+import icon from "astro-icon"
 import mdx from "@astrojs/mdx"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    icon({
+      include: {
+        fa: [
+          "github-square",
+          "external-link-square",
+          "facebook-square",
+          "twitter-square",
+          "linkedin-square",
+        ],
+        "simple-icons": ["astro"],
+      },
+    }),
     mdx(),
     tailwind({
       config: {
