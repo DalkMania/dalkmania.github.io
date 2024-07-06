cf053c803834915c33ade17dd8c330bed60c067c
Niklas Dahlqvist <dalkmania@gmail.com>
2024-07-06 15:13:34 +0200

    Fix Syntax Highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
   integrations: [
     icon({
       include: {
