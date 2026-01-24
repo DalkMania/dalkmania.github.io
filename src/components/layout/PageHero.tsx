288e4a8f0e31a7a94a5340d230b80eedee8058d7
Niklas Dahlqvist <dalkmania@gmail.com>
2026-01-23 23:55:04 -0800

    Fix Headings

--- src/components/layout/PageHero.tsx
+++ src/components/layout/PageHero.tsx
@@ -11,13 +11,10 @@ export const PageHero = () => {
         maxOpacity={0.1}
         duration={3}
         repeatDelay={1}
-        className={cn(
-          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
-          "inset-x-0 h-full skew-y-12"
-        )}
+        className={cn("mask-[radial-gradient(500px_circle_at_center,white,transparent)]", "inset-x-0 h-full skew-y-12")}
       />
       <div className="relative z-10 text-center max-w-3xl">
-        <h1 className="mt-6 text-4xl font-bold sm:text-5xl md:text-6xl !leading-[1.2] tracking-tight text-balance">
+        <h1 className="mt-6 text-4xl font-bold sm:text-5xl md:text-6xl leading-[1.2]! tracking-tight text-balance">
           Building Engaging Web Experiences
         </h1>
         <p className="mt-6 text-[17px] md:text-lg">
@@ -28,7 +25,7 @@ export const PageHero = () => {
         <div className="mt-12 flex items-center justify-center gap-4">
           <a href="/about/">
             <Button variant="outline" size="lg" className="rounded-full text-base shadow-none hover:cursor-pointer">
-              Learn More About Me <CircleArrowRight className="!h-5 !w-5" />
+              Learn More About Me <CircleArrowRight className="h-5! w-5!" />
             </Button>
           </a>
         </div>
