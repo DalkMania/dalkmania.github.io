c1814d61b6f964dc7e980504e713eaa27f2b184a
Niklas Dahlqvist <dalkmania@gmail.com>
2026-02-16 21:02:04 -0800

    Canonical urls

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.niklasdahlqvist.com/",
   vite: {
     // @ts-expect-error
     plugins: [tailwindcss()],
